881e133b797dd1fd5d2b57ec5b302b4854650123
Spookygh0 <143443467+Spookygh0@users.noreply.github.com>
2023-10-04 13:58:45 -0400

    Add files via upload

---
+++ P3HW2_SagadaCari.py
@@ -0,0 +1,36 @@
+#Name
+#Date 10/4/23
+#This program is going to use if statements to gross pay
+
+
+#Get input from user
+name = input("Enter employee name: ")
+num_hrs = float(input("Enter number of hours worked: "))
+pay_rate = float(input("Enter hourly pay rate: "))
+OT_hrs = 0
+has_OT = False
+OT_pay = 0
+
+#Calculate overtime - yes/no - how much
+if (num_hrs > 40):
+    has_OT = True
+    OT_hrs = num_hrs - 40
+else:
+    has_OT = False
+
+if has_OT == True:
+    OT_pay = pay_rate*1.5 * (num_hrs - 40) #Actual OT total pay
+else:
+    OT_pay = 0
+    
+
+#Calculate regular pay
+reg_pay = pay_rate * (num_hrs - OT_hrs)
+
+#Display name, pay rate, num hrs worked, OT hrs, OT pay, regular pay, gross pay
+
+print("Name: ", name)
+print("Overtime hours: ", OT_hrs)
+print("Overtime pay: ", OT_pay)
+print("Regular hours: ", num_hrs - OT_hrs)
+print("Regular pay: ", reg_pay + OT_pay)
